3fd21a0d841da9fb932e768647345fc46e4f1971
Burnedduk3 <juandavid.juandis@gmail.com>
2021-08-24 11:47:30 -0500

    added variables

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 variable instance_name {
     type = string
+    sensitive = true
+}
+
+variable AWS_ACCESS_KEY_ID {
+    type = string
+    sensitive = true
+}
+
+variable AWS_SECRET_ACCESS_KEY {
+    type = string
+    sensitive = true
+}
+
+variable AWS_DEFAULT_REGION {
+    type = string
+    sensitive = true
 }
 
 resource "aws_vpc" "main" {
