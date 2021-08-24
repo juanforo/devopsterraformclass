1ca03eaaeb26a491779d98702cc899ce5001e112
Burnedduk3 <juandavid.juandis@gmail.com>
2021-08-24 11:53:54 -0500

    dleted varioables

--- main.tf
+++ main.tf
@@ -3,21 +3,6 @@ variable instance_name {
     sensitive = true
 }
 
-variable AWS_ACCESS_KEY_ID {
-    type = string
-    sensitive = true
-}
-
-variable AWS_SECRET_ACCESS_KEY {
-    type = string
-    sensitive = true
-}
-
-variable AWS_DEFAULT_REGION {
-    type = string
-    sensitive = true
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
